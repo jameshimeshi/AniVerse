dca633668b1478046290625dba537b3e6cfed66c
jameshimeshi <jameshiyak@gmail.com>
2025-07-26 17:49:51 +0530

    changed default home

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,10 @@ function App() {
   }
 
    function tabswitchrandom(){
-    setTab(0);
+    setTab(1);
    }
     function tabswitchseasonal(){
-    setTab(1);
+    setTab(0);
    }
    function tabswitchTop(){
     setTab(2);
@@ -48,13 +48,16 @@ function App() {
   return (
     <>
     <div className='bg-black min-h-screen text-white'>
+
       <HeadComponent/>
         <div className="pt-24 flex justify-center">
               <div className="flex justify-center gap-4 flex-wrap w-[90%] mx-auto mt-6 mb-4">
       <div className="relative flex-1 min-w-[150px]">
+
          <button 
          className="w-full bg-indigo-600 text-white py-4 rounded-xl text-lg font-semibold pr-10 hover:bg-indigo-700 hover:scale-105 transition-transform duration-200 shadow-md flex justify-center items-center"
          onClick={tabswitchrandom}>Random</button>
+
         {tab === 0 && (
         <button onClick={fetchNewRandomAnime} 
          className="absolute right-2 top-1/2 -translate-y-1/2 text-white rounded-full p-1 w-7 h-7 flex justify-center items-center shadow-md hover:bg-red-400 hover:scale-140 cursor-pointer transition-transform"
@@ -63,14 +66,15 @@ function App() {
         </button>)}
      </div>
       <div className="w-full sm:w-1/3">
-          <button className='w-full bg-indigo-600 text-white py-4 rounded-xl text-lg font-semibold hover:bg-indigo-700 hover:scale-105 transition-transform duration-200 shadow-md' onClick={tabswitchseasonal}>Seasonal</button>
+          <button className='w-full bg-indigo-600 text-white py-4 rounded-xl text-lg font-semibold hover:bg-indigo-700 hover:scale-105 transition-transform duration-200 shadow-md' onClick={tabswitchseasonal}>
+          Seasonal</button>
           </div>
             <div className="w-full sm:w-1/3">
           <button className='w-full bg-indigo-600 text-white py-4 rounded-xl text-lg font-semibold hover:bg-indigo-700 hover:scale-105 transition-transform duration-200 shadow-md' onClick={tabswitchTop}>Top</button>
           </div>
           </div></div>
-         {tab===1 && <SeasonalAnime/>}
-         {tab===0 && <RandomAnime key={random?.mal_id} random={random} loading={loading}/>}
+         {tab===0 && <SeasonalAnime/>}
+         {tab===1 && <RandomAnime key={random?.mal_id} random={random} loading={loading}/>}
          {tab===2 && <TopAnime/>}
          <FootComponent/>
          
